8d20654db3ca96736ead723feda7bae370cd824d
mateus-pereira-ezops <mateus-pereira@ezops.cloud>
2026-03-11 17:02:53 -0300

    feat: add new outputs to ecs

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -2,7 +2,12 @@ output "alb_dns" {
   value = aws_lb.main.dns_name
 }
 
+output "alb_zone_id" {
+  value = aws_lb.main.zone_id
+}
+
 output "db_host" {
   value     = var.db_host
   sensitive = true
 }
+
