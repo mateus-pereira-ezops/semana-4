094a87c3f674d8fbb1ebd83306b8138c67a01624
mateus-pereira-ezops <mateus-pereira@ezops.cloud>
2026-03-11 17:07:55 -0300

    feat: add acm service

---
+++ modules/acm/main.tf
@@ -0,0 +1,33 @@
+resource "aws_acm_certificate" "main" {
+  domain_name       = var.subdomain
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Project = var.project_name
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = var.hosted_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
---
+++ modules/acm/variables.tf
@@ -0,0 +1,12 @@
+variable "project_name" {
+  type = string
+}
+
+variable "subdomain" {
+  type    = string
+  default = "mpdesafio4.ezopscloud.co"
+}
+
+variable "hosted_zone_id" {
+  type = string
+}
